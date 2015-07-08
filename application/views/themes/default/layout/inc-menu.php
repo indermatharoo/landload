a375616c293dbbfe70d6d420bc05b0cbf9809fe2
BalwinderMcc <balwinder@multichannelcreative.co.uk>
2015-07-08 11:33:07 +0530

    design implemented in site

--- application/views/themes/default/layout/inc-menu.php
+++ application/views/themes/default/layout/inc-menu.php
@@ -1,22 +1,27 @@
-<nav class="navbar ">
-    <div class="container-fluid padding-0">
-        <div class="navbar-header">
-            <button aria-controls="navbar" aria-expanded="false" data-target="#navbar" data-toggle="collapse" class="navbar-toggle collapsed pull-right" type="button">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="fa fa-bars fa-2x"></span>
-            </button>
+<div class="navbar-header">
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+    <div class="logo_container">
+        <div class="logo">
+            <a href="<?php echo base_url(); ?>">
+                <img src="imgs/logo.png" class="img-responsive"/>
+            </a>
         </div>
-        <div class="navbar-collapse collapse" id="navbar">
-            <?php
-            $params = array(
-                'menu_alias' => 'main_menu',
-                'ul_format' => '<ul class="nav navbar-nav navbar-right">{MENU}</ul>',
-                'level_1_format' => '<a href="{HREF}"{ADDITIONAL}><span>{LINK_NAME}</span></a>',
-                'level_2_format' => '<a href="{HREF}"{ADDITIONAL}><span>{LINK_NAME}</span></a>'
-            );
-            
-            echo $CI->html->menu($params);
-            ?>
-        </div><!--/.nav-collapse -->
-    </div><!--/.container-fluid -->
-</nav>
+    </div>
+</div>
+<div id="navbar" class="navbar-collapse collapse top_mymenu">
+    <?php
+    $params = array(
+        'menu_alias' => 'header_menu',
+        'ul_format' => '<ul class="nav navbar-nav navbar-right">{MENU}</ul>',
+        'level_1_format' => '<a href="{HREF}"{ADDITIONAL}><span>{LINK_NAME}</span></a>',
+        'level_2_format' => '<a href="{HREF}"{ADDITIONAL}><span>{LINK_NAME}</span></a>'
+    );
+
+    echo $CI->html->menu($params);
+    ?>
+</div><!--/.nav-collapse -->
