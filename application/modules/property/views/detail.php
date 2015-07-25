9c90c95147a64dba2a16c7f07a2aa2c8aaf38e0d
unknown <balwinder@multichannelcreative.co.uk>
2015-07-25 17:11:11 +0530

    property detail

--- application/modules/property/views/detail.php
+++ application/modules/property/views/detail.php
@@ -3,7 +3,6 @@
 ?>
 <div class="row">
     <div class="col-lg-8">
-        <?php if(count($gallery)!=0):?>
         <div class="col-lg-12 padding-0 unit-imgs" style="height: 300px;">
             <div id="carousel-example" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner" style="height: 292px;">
@@ -32,7 +31,6 @@
                 </a>
             </div>
         </div>
-        <?php endif;?>
         <div class="col-lg-12 padding-0 mar-top30">
             <div class="head_desc">
                 <h2><?php echo $property['unit_number']; ?></h2>
