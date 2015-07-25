68ead6e96d76f7275a1c30a5ddfd5844a2c878ba
indermatharoo <indersmatharoo@gmail.com>
2015-07-25 18:12:12 +0530

    local changes

--- application/admin/modules/invoice/views/invoice_template.php
+++ application/admin/modules/invoice/views/invoice_template.php
@@ -259,9 +259,8 @@ function vat($price_without_vat = NULL) {
 <?php
 echo arrIndex($invoice_detail, 'invoice_code');
 $invoice_code = arrIndex($invoice_detail, 'invoice_code');
-e($this->aauth);
 ?>
-<?php if ($this->aauth->isCustomer()): ?>
+<?php if ($this->aauth->isCustomer() && $invoice_code): ?>
     <button class="btn btn-success mar-left10" onclick="payInvoice()">Pay Online</button>
 <?php endif; ?>
 
