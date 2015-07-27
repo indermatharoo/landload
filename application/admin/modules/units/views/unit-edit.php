40e138a40d1ac62a7b1e37eef2efc5b061999189
rohitmcc <rohit@multichannelcreative.com>
2015-07-27 14:06:14 +0530

    remove from to from report

--- application/admin/modules/units/views/unit-edit.php
+++ application/admin/modules/units/views/unit-edit.php
@@ -7,7 +7,7 @@
             <i class="fa fa-user fa-2x"></i>
         </div>
         <div class="col-sm-10">
-            <h3 style="margin: 0; text-align: center">Edit Unit</h3>
+            <h3 style="margin: 0; text-align: center">Edit Property</h3>
         </div>
         <div class="col-sm-1" style="text-align: right">
             <a href="units"><h3 style="cursor: pointer; margin: 0; color: #fff"><i class="fa fa-home" title="Manage Units"></i></h3></a>
